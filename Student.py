bea44d052e8eecbd12a377f94fa8454de3ce650a
AdinSolomon <adinbsolomon@gmail.com>
2021-03-21 21:33:28 -0400

    Added CAS outline for course requirements

--- Student.py
+++ Student.py
@@ -1,4 +1,6 @@
 
+import Requirement
+
 # This class is used to represent a student's status
 # - what courses have been completed
 # - currently enrolled courses
@@ -7,7 +9,12 @@ class Student:
     def __init__(self, **kwargs):
         self.name = kwargs.get("name")
         self.enrollment_year = kwargs.get("enrollment_year")
-        self.completed_courses = kwargs.get("completed_courses")
-        self.current_courses = kwargs.get("current_courses")
+        self.completed_courses:dict = kwargs.get("completed_courses")
+        self.current_courses:dict = kwargs.get("current_courses")
+    # Requirement methods
+    def meets_prerequisite(self, prereq:Requirement.Requirement) -> bool:
+        return prereq.satisfied(self.completed_courses)
+    def meets_corequisite(self, coreq:Requirement.Requirement) -> bool:
+        return coreq.satisfied(self.current_courses)
         
         
